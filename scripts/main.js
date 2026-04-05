de9c14979cb5ea3cbdfcade2e738b626c181cfdf
Lada Orlova <kleshnya3000@gmail.com>
2026-04-05 20:48:33 +0400

    feat: save a new task

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,31 @@
 import { saveTasks, loadTasks } from "./storage.js";
 
-const test = { text: "пупупу" };
-saveTasks(test);
-console.log("Сохранено");
+const addButton = document.getElementById("addButton"); // переменная для кнокпи добавления задачи
 
-const loaded = loadTasks();
-console.log("Загружено из базы:", loaded);
+let myTasks = loadTasks(); //загружает в массив данные из бд 
+// newTask — это объект (одна карточка), а myTasks — это массив (стопка карточек)
+
+addButton.onclick = function (event) { // событие по клику на кнопку
+  event.preventDefault(); // запрет на обновление страницы при нажатии на кнопку добавить
+  let taskName = document.getElementById("taskName").value.trim(); // получение и обрезка лишних пустых симовлов из инпута
+  if (!taskName) return; // проверка на пустой инпут
+
+  let now = new Date(); //создание переменной в которую записывается дата текущая
+  const newTask = { // объект новой задачи
+    id: crypto.randomUUID(), // генерация ID
+    taskName, 
+    year: now.getFullYear(), // добавление именно года, месяца, даты и дня (строки ниже)
+    month: now.getMonth(),
+    date: now.getDate(),
+    day: now.getDay(),
+    isDone: false, // задача не выполнена по умолчанию
+  };
+
+  myTasks.push(newTask); // добавление новой задачи в наш массив
+  saveTasks(myTasks); // сохранение объекта
+
+  document.getElementById("taskName").value = ""; // очистка инпута
+
+  // временная проверка
+  console.log(myTasks);
+};
