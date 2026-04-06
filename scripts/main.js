288977d12ad62bcfdb6c8670a1f915c1fb9ccbac
Lada Orlova <kleshnya3000@gmail.com>
2026-04-06 15:59:09 +0400

    feat: dysplaing data

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
 import { saveTasks, loadTasks } from "./storage.js";
+import { displayList } from "./ui.js";
 
 const addButton = document.getElementById("addButton"); // переменная для кнокпи добавления задачи
 
-let myTasks = loadTasks(); //загружает в массив данные из бд 
+let myTasks = loadTasks(); //загружает в массив данные из бд
 // newTask — это объект (одна карточка), а myTasks — это массив (стопка карточек)
+displayList(myTasks); //отрисвка уже имеющихся задач
 
-addButton.onclick = function (event) { // событие по клику на кнопку
+addButton.onclick = function (event) {
+  // событие по клику на кнопку
   event.preventDefault(); // запрет на обновление страницы при нажатии на кнопку добавить
   let taskName = document.getElementById("taskName").value.trim(); // получение и обрезка лишних пустых симовлов из инпута
   if (!taskName) return; // проверка на пустой инпут
 
   let now = new Date(); //создание переменной в которую записывается дата текущая
-  const newTask = { // объект новой задачи
+  const newTask = {
+    // объект новой задачи
     id: crypto.randomUUID(), // генерация ID
-    taskName, 
+    taskName,
     year: now.getFullYear(), // добавление именно года, месяца, даты и дня (строки ниже)
     month: now.getMonth(),
     date: now.getDate(),
@@ -25,7 +29,7 @@ addButton.onclick = function (event) { // событие по клику на к
   saveTasks(myTasks); // сохранение объекта
 
   document.getElementById("taskName").value = ""; // очистка инпута
-
+  displayList(myTasks);
   // временная проверка
   console.log(myTasks);
 };
---
+++ scripts/ui.js
@@ -0,0 +1,19 @@
+export function displayList(myTasks) {
+  const list = document.querySelector(".to-do-list"); // добаляем ul в переменную
+  list.innerHTML = ""; // очищаем отрисовку в начале функции, чтобы задачи не множились
+
+  //перебираем задачи
+  myTasks.forEach((task) => {
+    const item = document.createElement("li"); //создаем li
+    item.className = "to-do-item"; // задаем класс для li
+
+    //вставляем в li информацию из объекта
+    item.innerHTML = ` 
+    <input type="checkbox">
+    <span>${task.taskName}</span>
+    <button class="delete-button">Delete</button>
+    `;
+
+    list.appendChild(item); // присваевам li родителя list (ul)
+  });
+}
